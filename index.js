ee2e10b4e571860d204cb1fed74b3a9847563b06
maktubhelou <markallanevans@gmail.com>
2018-04-23 21:15:08 +0700

    updating content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ Greetings: 'this is the First Deployment of a new App' });
+	res.send({ "Greetings": "this is the First Deployment of a new App", "Farewell": "You have nothing to lose by coming back, except time!" });
 });
 
 const PORT = process.env.PORT || 5000;
